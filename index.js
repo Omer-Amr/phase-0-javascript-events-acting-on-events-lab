99c809edfe9779d9dbb80ba949fe96716b603284
omer <you@example.com>
2022-12-15 16:54:26 +0300

    completed assignmnt

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodgerLeft = document.getElementById("dodger");
+const dodgerRight = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodgerLeft.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodgerLeft.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (x) {
+    if (x.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerRight() {
+    const rightNumbers = dodgerRight.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+
+    if(left < 360){
+        dodgerRight.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(y){
+    if(y.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+})
